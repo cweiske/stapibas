cfb679840a7596a24527a7f505728f7f9a1a6118
Christian Weiske <cweiske@cweiske.de>
2014-03-26 23:40:45 +0100

    disable ssl verification when validating linkback request

--- www/xmlrpc.php
+++ www/xmlrpc.php
@@ -6,7 +6,20 @@ namespace stapibas;
 require_once 'www-header.php';
 
 $s = new \PEAR2\Services\Linkback\Server();
-$s->addCallback(new Linkback_DbStorage($db));
-$s->addCallback(new Linkback_Mailer());
+
+$fs = new \PEAR2\Services\Linkback\Server\Callback\FetchSource();
+$fs->getRequest()->setConfig(
+    array(
+        'ssl_verify_peer' => false,
+        'ssl_verify_host' => false
+    )
+);
+$callbacks = array(
+    $fs,
+    new \PEAR2\Services\Linkback\Server\Callback\LinkExists(),
+    new Linkback_DbStorage($db),
+    new Linkback_Mailer()
+);
+$s->setCallbacks($callbacks);
 $s->run();
 ?>
